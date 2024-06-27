eaf053a5332767ec512fe3aaabe828862f52c539
Antek <pochopienantek@gmail.com>
2024-06-27 12:29:01 +0200

    added page counter

--- lib/common/widgets/just_pdf_app_bar.dart
+++ lib/common/widgets/just_pdf_app_bar.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:just_pdf/l10n/l10n.dart';
 
 class JustPdfAppBar extends StatelessWidget implements PreferredSizeWidget{
-  const JustPdfAppBar({super.key});
+  final List<Widget>? actions;
+  const JustPdfAppBar({super.key, this.actions});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(T(context).app_name),
       notificationPredicate: (_) => false,
+      actions: actions,
     );
   }
   
--- lib/pdf_viewer/pdf_viewer_screen.dart
+++ lib/pdf_viewer/pdf_viewer_screen.dart
@@ -5,17 +5,33 @@ import 'package:just_pdf/common/widgets/just_pdf_app_bar.dart';
 import 'package:just_pdf/dashboard/domain/file_metadata.dart';
 
 @RoutePage()
-class PdfViewerScreen extends StatelessWidget {
+class PdfViewerScreen extends StatefulWidget {
   final FileMetadata fileMetadata;
   const PdfViewerScreen({super.key, required this.fileMetadata});
 
   @override
+  State<PdfViewerScreen> createState() => _PdfViewerScreenState();
+}
+
+class _PdfViewerScreenState extends State<PdfViewerScreen> {
+  int? _pagesCount;
+  int _currentPage = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const JustPdfAppBar(),
+      appBar: JustPdfAppBar(
+        actions: [if (_pagesCount != null) Text('${_currentPage+1}/$_pagesCount')],
+      ),
       body: PDFView(
-      filePath: fileMetadata.filePath,
-    ),
+        filePath: widget.fileMetadata.filePath,
+        onPageChanged: (page, total) {
+          setState(() {
+            _currentPage = page!;
+            _pagesCount = total;
+          });
+        },
+      ),
     );
   }
 }
