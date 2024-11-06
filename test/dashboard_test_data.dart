9fad8864fc40dd13825dd75c0191c837f030aaa8
Antoni Pochopien <pochopienantek@gmail.com>
2024-11-06 01:48:47 +0100

    created dashboard tests for screen loaded, and A-Z sorting

---
+++ test/dashboard_test_data.dart
@@ -0,0 +1,20 @@
+import 'dart:math' as math;
+
+import 'package:just_pdf/dashboard/domain/file_metadata.dart';
+
+String _generateRandomPath(int index) {
+  final random = math.Random(index);
+  String randomLetters(int length) => String.fromCharCodes(
+      List.generate(length, (_) => random.nextInt(26) + 97));
+  return '${randomLetters(10)}/${randomLetters(10)}';
+}
+
+final dashboardDataFiles = List<FileMetadata>.generate(100, (index) {
+  final lastViewed = DateTime.now().subtract(Duration(days: index));
+  return FileMetadata(
+      id: index.toString(),
+      filePath: _generateRandomPath(index),
+      lastViewed: lastViewed,
+      sizeInBytes: index + 1);
+})
+  ..shuffle();
